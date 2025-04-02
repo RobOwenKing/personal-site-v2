6cebd5ab0925cd34b03b4c12cc00ab373771d321
Robert King <robert.o.king@hotmail.co.uk>
2025-04-02 23:46:29 +0100

    feat: Migrate db to add Tune model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_29_230635) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_06_231943) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tunes", force: :cascade do |t|
+    t.string "title"
+    t.string "composer"
+    t.string "meter"
+    t.string "basic_note_length"
+    t.string "key"
+    t.string "abc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
